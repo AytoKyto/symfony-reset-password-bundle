62b25e6ef85d134126ebc0bae539b6b2b6e705e9
fleury_mathis <mathis@lanageuse.com>
2024-12-07 14:24:17 +0100

    🐛 correction namespace

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Mathfle\ResetPasswordBundle\DependencyInjection;
+namespace Ayto\ResetPasswordBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
